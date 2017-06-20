615671b2df2eb32e5bc4234adb18ee2c08160841
sharmamano <manojkumar.sharma@in.fujitsu.com>
2017-06-20 18:38:43 +0530

    hi

---
+++ AppCleanNew/src/MainCalssMethod.java
@@ -0,0 +1,16 @@
+/**
+ * Hello world!
+ *
+ *
+ */
+public class MainCalssMethod
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "start" );
+        System.exit( -1 );
+        System.out.println( "end" );
+    }
+
+}
